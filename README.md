b093908b4b78cc40edcba9be9e9a58911e54492f
Radostin Minchev <tinchoRSM@gmail.com>
2022-05-19 16:21:08 +0300

    Innitial commint and set up sever structure.

---
+++ README.md
@@ -0,0 +1,14 @@
+# User Emplyoees 
+
+A basic REST CRUD App
+
+- Create a database
+- Login form User email password
+- Visulize employees
+- Adding employees
+- Editing employees
+- Deleting employees
+
+
+
+
---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.listen(port, () =>{
+    console.log(`Server started on port ${port}`);
+});
