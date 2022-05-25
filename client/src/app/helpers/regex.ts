af7eb28217a15fce4267fb0671605556135072cb
Radostin Minchev <tinchoRSM@gmail.com>
2022-05-25 23:53:59 +0300

    Added visual changes.

--- client/src/app/helpers/regex.ts
+++ client/src/app/helpers/regex.ts
@@ -1,6 +1,6 @@
 export const regexValidators = {
     email: "^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$",
-    name: "",
+    name: "(^[A-Za-z]{3,16})([ ]{0,1})([A-Za-z]{3,16})?([ ]{0,1})?([A-Za-z]{3,16})?([ ]{0,1})?([A-Za-z]{3,16})",
     password: "",
     phone: "",
     salary: ""
